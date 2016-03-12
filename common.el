7a93985df7b7a338747564f36aec130da35b1980
Konstantin Zudov <co@zudov.me>
2016-03-12 20:36:05 +0200

    Remap execution bindings; Add keyfreq;

--- common.el
+++ common.el
@@ -2,3 +2,16 @@
  'helm)
 
 (helm-mode 1)
+
+;; Some global key translations
+
+(define-key key-translation-map [?\C-x] [?\C-u])
+(define-key key-translation-map [?\C-u] [?\C-x])
+
+(define-key key-translation-map [?\M-x] [?\M-u])
+(define-key key-translation-map [?\M-u] [?\M-x])
+
+(ensure-package-installed 'keyfreq)
+(require 'keyfreq)
+(keyfreq-mode 1)
+(keyfreq-autosave-mode 1)
