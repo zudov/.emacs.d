4594eb0d2ae5cf1a1ead9cdddf9c98a1da9cf426
Konstantin Zudov <co@zudov.me>
2016-03-28 19:36:52 +0300

    Avy

--- common.el
+++ common.el
@@ -11,7 +11,17 @@
 (define-key key-translation-map [?\M-x] [?\M-u])
 (define-key key-translation-map [?\M-u] [?\M-x])
 
+
+
 (ensure-package-installed 'keyfreq)
 (require 'keyfreq)
 (keyfreq-mode 1)
 (keyfreq-autosave-mode 1)
+
+
+(ensure-package-installed 'avy 'ace-window)
+
+(global-set-key (kbd "C-.") 'ace-window)
+(global-set-key (kbd "C-,") 'avy-goto-word-or-subword-1)
+(setq aw-keys '(?u ?h ?e ?t ?o ?n ?a ?s))
+(setq avy-keys '(?u ?h ?e ?t ?o ?n ?a ?s))
