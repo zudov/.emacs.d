6d40311490ed6315f6a45b8889e1ee9130e7c56e
Konstantin Zudov <co@zudov.me>
2016-06-10 22:42:48 +0300

    Use intero-mode

--- haskell.el
+++ haskell.el
@@ -1,16 +1,23 @@
 (use-package haskell-mode
+  :ensure t
   :init (progn
-	  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+	  ;(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 	  (custom-set-variables
 	   '(haskell-process-type 'stack-ghci)
 	   '(haskell-process-auto-import-loaded-modules t)
 	   '(haskell-process-log t)
 	   '(haskell-tags-on-save t)))
-  :config (progn
-	    (require 'haskell-interactive-mode)
-	    (require 'haskell-process))
-  :bind (:map haskell-mode-map
-	      ("C-c C-l" . haskell-process-load-or-reload)
-	      ("C-`" . haskell-interactive-bring)
-	      ("M-." . haskell-mode-jump-to-def-or-tag)
-	      ("M-," . xref-pop-marker-stack)))
+;  :config (progn
+;	    (require 'haskell-interactive-mode)
+;	    (require 'haskell-process))
+;  :bind (:map haskell-mode-map
+;	      ("C-c C-l" . haskell-process-load-or-reload)
+;	      ("C-`" . haskell-interactive-bring)
+;	      ("M-." . haskell-mode-jump-to-def-or-tag)
+;	      ("M-," . xref-pop-marker-stack))
+  )
+
+(use-package intero
+  :ensure t
+  :init (progn
+	  (add-hook 'haskell-mode-hook 'intero-mode)))
