bde20196d0242ee33c451ae1ef398a8b8f432685
Konstantin Zudov <co@zudov.me>
2017-04-19 22:58:33 +0300

    Purescript: use psc-ide

--- purescript.el
+++ purescript.el
@@ -5,3 +5,10 @@
 (use-package psci
   :ensure t
   :pin emacs-pe)
+
+(use-package psc-ide
+  :ensure t
+  :init (progn
+	  (add-hook 'purescript-mode-hook 'psc-ide)
+	  (add-hook 'purescript-mode-hook 'company-mode)
+	  (add-hook 'purescirpt-mode-hook 'flycheck-mode)))
